acc0a043f97b2ff6467fcaf56918525ca84f57e0
Svetozar Stoev <zarkoto23@gmail.com>
2026-03-26 16:42:01 +0200

    task 06

---
+++ course-tasks/06.ts
@@ -0,0 +1,19 @@
+class KeyValuePair<T, U> {
+  private key!: T;
+  private val!: U;
+
+  setKeyValue(key: T, val: U): void {
+    this.key = key;
+    this.val = val;
+  }
+
+  display(): void {
+    console.log(`key = ${this.key}, value = ${this.val}`);
+  }
+}
+
+let kvp = new KeyValuePair<number, string>();
+
+kvp.setKeyValue(1, "Steve");
+
+kvp.display();
