cd887326f9e418cfc7b3b16be6b457036a6a9535
Svetozar Stoev <zarkoto23@gmail.com>
2026-04-02 19:41:08 +0300

    add first component

--- angular-components/components-app/src/app/app.ts
+++ angular-components/components-app/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponent } from './first-component/first-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FirstComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ angular-components/components-app/src/app/first-component/first-component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  imports: [],
+  templateUrl: './first-component.html',
+  styleUrl: './first-component.css',
+})
+export class FirstComponent {
+
+   name='PEPI'
+
+
+  constructor(){
+    console.log('KHJAHS');
+    
+  }
+
+
+}
